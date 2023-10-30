362df5f3de53c3ee6a758c9c3ae24d2c025f7776
jahid4striver <jahidhassan701@gmail.com>
2023-10-30 21:15:48 +0600

    nested and dynamic route created

---
+++ src/app/news/page.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NewsPage = () => {
+    return (
+        <div>
+            <h1>This is News Page
+            </h1>
+        </div>
+    );
+};
+
+export default NewsPage;
--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,9 @@
+"use client"
 import React from 'react';
 
+
 const HomePage = () => {
+  console.log("This is From Server");
   return (
     <div>
       <h1 className='text-6xl'>This is Homepage</h1>
